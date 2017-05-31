dc3ee3b6dc8fab29816036f2ac21688c93b611ef
Christian Lewe <ch_lewe@wh2.tu-dresden.de>
2017-05-31 17:53:02 +0200

    Added Haddock documentation

--- NGramPredict.hs
+++ NGramPredict.hs
@@ -4,6 +4,8 @@ import Control.Monad (replicateM_)
 -- import System.Environment (getArgs)
 import System.IO
 
+-- | Entry point of the application.
+-- Console arguments: number, model, file, line, column
 main :: IO [()]
 main = do
     --args <- getArgs
@@ -13,8 +15,13 @@ main = do
     mapM putStrLn prefix
 
 
--- file -> length of prefix -> line -> column -> prefix
-getPrefix :: Handle -> Int -> Int -> Int -> IO [String]
+-- | Returns the prefix for the word suggestion.
+getPrefix ::
+    Handle ->   -- ^ handle of text file
+    Int ->      -- ^ length of prefix
+    Int ->      -- ^ line number (1-based)
+    Int ->      -- ^ column number (1-based)
+    IO [String] -- ^ found prefix
 getPrefix file prefixLen line col = do
     replicateM_ (line - 1) $ hGetLine file
     foundLine <- hGetLine file
