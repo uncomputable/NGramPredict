62d66ffc8efef5ede719d67cdfd42d4670fcb073
Christian Lewe <ch_lewe@wh2.tu-dresden.de>
2017-05-31 17:54:49 +0200

    Added support for command line arguments

--- NGramPredict.hs
+++ NGramPredict.hs
@@ -1,18 +1,39 @@
 module Main where
 
 import Control.Monad (replicateM_)
--- import System.Environment (getArgs)
+import System.Environment (getArgs, getProgName)
 import System.IO
 
 -- | Entry point of the application.
 -- Console arguments: number, model, file, line, column
-main :: IO [()]
+main :: IO ()
 main = do
-    --args <- getArgs
-    --mapM putStrLn args
-    file <- openFile "text.txt" ReadMode
-    prefix <- getPrefix file 2 2 12
-    mapM putStrLn prefix
+    args <- getArgs
+    if length args < 5
+    then printHelp
+    else do
+        let fileName = args !! 2
+        let line = read $ args !! 3
+        let colomn = read $ args !! 4
+
+        file <- openFile fileName ReadMode
+        prefix <- getPrefix file 2 line colomn
+        mapM_ putStrLn prefix
+
+
+-- | Prints usage advice on the command line.
+printHelp :: IO ()
+printHelp = do
+    progName <- getProgName
+    mapM_ putStrLn
+        [ progName ++ " <number> <model> <file> <line> <column>"
+        , "  where"
+        , "    <number> ... number of word suggestions"
+        , "    <model>  ... path to ARPA file with n-gram model"
+        , "    <file>   ... path to text file that is to be analysed"
+        , "    <line>   ... line number for suggestion position (1-based)"
+        , "    <column> ... column number for suggestion position (1-based)"
+        ]
 
 
 -- | Returns the prefix for the word suggestion.
