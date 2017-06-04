9d5582ccc3136c38c8d02f34bc7e75208129ca23
Christian Lewe <ch_lewe@wh2.tu-dresden.de>
2017-06-04 22:57:22 +0200

    Added useful Prob and Model functions

--- Model.hs
+++ Model.hs
@@ -7,3 +7,25 @@ data Header = Header Int [Int] deriving Show
 data Prob = Prob Double Double | ProbMax Double deriving Show
 type NGrams = Map.Map [String] Prob
 data Model = Model Header [NGrams] deriving Show
+
+-- | Extracts the list of all n-grams from a model.
+extractAllNGrams ::
+    Model -> -- ^ source of to-be-extracted list
+    [NGrams] -- ^ extracted list of n-grams
+extractAllNGrams (Model _ allNGrams) = allNGrams
+
+
+-- | Extracts the probability from a Prob structure.
+extractProb ::
+    Prob -> -- ^ source of to-be-extracted probability
+    Double  -- ^ extracted probability
+extractProb (Prob p _) = p
+extractProb (ProbMax p) = p
+
+
+-- | Extracts the backoff weight from a Prob structure.
+extractBackoff ::
+    Prob -> -- ^ source of to-be-extracted backoff weight
+    Double  -- ^ extracted backoff weight
+extractBackoff (Prob _ b) = b
+extractBackoff _ = undefined
