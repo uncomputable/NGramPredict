e10558a90c62f16cb202a402b9620fcfb253ebba
Christian Lewe <ch_lewe@wh2.tu-dresden.de>
2017-06-14 23:49:01 +0200

    Adapted Probability module to changes
    I smell compiling repo code!

--- Probability.hs
+++ Probability.hs
@@ -2,9 +2,9 @@
 module Probability (predict) where
 
 import Model
-import qualified Data.Map.Strict as Map
 import Data.List (foldl')
-import Data.Maybe (isNothing)
+import qualified Data.Map.Strict as Map
+import Data.Maybe (fromJust, isNothing)
 
 -- | Returns the n most probable unigrams following a given prefix and using
 -- a specific language model.
@@ -14,12 +14,19 @@ predict
     -> Model     -- ^ language model
     -> [String]  -- ^ list of n unigrams u with highest p(u | p)
 predict n prefix model =
-    let allNGrams = extractAllNGrams model
-        oneGrams = head allNGrams
-        wordsWithProbs = map (\[u] -> (u, computeProb u prefix allNGrams)) $ Map.keys oneGrams
-                           --  ^^^ non-exhaustive pattern matching?
-    in map fst $ foldl' selectBestN [] wordsWithProbs
+    let mapping = modelUniMap model
+        encPrefix = map (fromJust (error "Err") . (`Map.lookup` mapping)) prefix
+    in map show $ go encPrefix
     where
+        go :: [Integer] -> [Integer]
+        go encPrefix =
+            let allNGrams = modelNGrams model
+                uniGrams = Map.keys $ head allNGrams
+                uniGramsWithProbs =
+                    map (\[u] -> (u, computeProb u encPrefix allNGrams)) uniGrams
+                      --  ^^^ non-exhaustive pattern matching?
+            in map fst $ foldl' selectBestN [] uniGramsWithProbs
+
         selectBestN :: Ord b => [(a, b)] -> (a, b) -> [(a, b)]
         selectBestN best curr
             | length best < n = curr : best
@@ -35,23 +42,23 @@ predict n prefix model =
 -- | Computes the probablity of a word following a specific prefix, using a
 -- specific language model.
 computeProb
-    :: String    -- ^ word w_i following prefix
-    -> [String]  -- ^ prefix p
+    :: Integer   -- ^ word w_i following prefix (encoded)
+    -> [Integer] -- ^ prefix p (encoded)
     -> [NGrams]  -- ^ list of maps of n-grams
     -> Double    -- ^ probability (w_i | p)
 computeProb w_i fullPrefix allNGrams = (10 **) $ go $ fullPrefix ++ [w_i]
     where
-        go :: [String] -> Double
-        go ngram = let prob = getProb ngram
-                   in maybe (backoff ngram) extractProb prob
+        go :: [Integer] -> Double
+        go ngram = let maybeProb = getProb ngram
+                   in maybe (goBackoff ngram) problty maybeProb
 
-        backoff :: [String] -> Double
-        backoff [] = undefined -- unigram w_i was not in model at all! FIXME: needs error message
-        backoff (_ : shorter) = let prob = getProb shorter
-                                    b = maybe 0 extractBackoff prob
-                                in b + go shorter
+        goBackoff :: [Integer] -> Double
+        goBackoff [] = undefined -- unigram w_i was not in model at all! FIXME: needs error message
+        goBackoff (_ : shorter) = let maybeProb = getProb shorter
+                                      weight = maybe 0 backoff maybeProb
+                                  in weight + go shorter
 
-        getProb :: [String] -> Maybe Prob
+        getProb :: [Integer] -> Maybe Prob
         getProb ngram = let probs = map (Map.lookup ngram) allNGrams
                         in firstJust probs
                         -- thank you, lazyness!
